41ee74fd96f3188aa5e4b7c8a235c7f19cd76fcd
rmoayinla <rmoayinla@yahoo.com>
2017-09-09 14:45:19 +0100

    Now defined the runModule method to run added modules or passed modules

--- includes/trait-logger-module.php
+++ includes/trait-logger-module.php
@@ -9,6 +9,7 @@
 namespace includes;
 
 trait Trait_Logger_Module{
+	
 	/**
 	 * Add modules that will extend the $logger
 	 * each module is an instance of includes\Logger_module
@@ -22,7 +23,7 @@ trait Trait_Logger_Module{
 
 	public function getModule( Abstract_Logger_Module $module = null ){
 		
-		if( empty( $module ) ) return $this->modules; //return array of all modules if none is passed //
+		if( empty( $module ) ) return array_shift( $this->modules ); //return the first module if none is passed //
 
 		if( !in_array( $module, $this->modules ) && !array_key_exists( $module->toString(), $this->modules ) )
 			throw new \Exception( __( "Module cannot be found", "abbey-wp-error" ) );
@@ -30,7 +31,24 @@ trait Trait_Logger_Module{
 		return $this->modules[ $module->toString() ];
 	} 
 
+	/**
+	 * Run added modules or an indivdual module
+	 * Modules are run just by calling the run method on the module object 
+	 */
 	public function runModule( Abstract_Logger_Module $module = null ){
-
+		$module_to_run = null;
+		if( !empty( $module ) ){
+			if( $module_to_run = $this->getModule( $module ) )
+				return $module_to_run->run( $this->logger );
+			return;
+		}
+		
+		if( empty( $this->modules ) || !is_array( $this->modules ) ) 
+			throw new \Exception( __( "Module have not been added yet", "abbey-wp-error" ) );
+		
+		foreach( $this->modules as $modules ){
+			$modules->run( $this->logger );
+		}
 	}
+
 }
