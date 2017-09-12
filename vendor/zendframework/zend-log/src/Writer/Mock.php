de0ac8667932ebf8acd74e0fb70448cc9aab8b39
rmoayinla <rmoayinla@yahoo.com>
2017-09-12 18:38:07 +0100

    Default writers for Zend Log

---
+++ vendor/zendframework/zend-log/src/Writer/Mock.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link      http://github.com/zendframework/zend-log for the canonical source repository
+ * @copyright Copyright (c) 2005-2016 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace Zend\Log\Writer;
+
+class Mock extends AbstractWriter
+{
+    /**
+     * array of log events
+     *
+     * @var array
+     */
+    private $events = [];
+
+    /**
+     * shutdown called?
+     *
+     * @var bool
+     */
+    public $shutdown = false;
+
+    /**
+     * Write a message to the log.
+     *
+     * @param array $event event data
+     * @return void
+     */
+    public function doWrite(array $event)
+    {
+        $this->events = $event;
+    }
+
+    public function getErrorEvents(){
+        return !empty( $this->events ) ? $this->events : false;
+    }
+
+    /**
+     * Record shutdown
+     *
+     * @return void
+     */
+    public function shutdown()
+    {
+        $this->shutdown = true;
+    }
+}
