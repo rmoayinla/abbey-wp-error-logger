7648f7c969d62e441a8b4b165ac112c9f5a6062f
rmoayinla <rmoayinla@yahoo.com>
2017-10-10 09:01:50 +0100

    A zend logger processor that adds wordpress like events and posts info for the current request which an error is logged on

--- vendor/zendframework/zend-log/src/Processor/WPEvent.php
+++ vendor/zendframework/zend-log/src/Processor/WPEvent.php
@@ -13,35 +13,40 @@ class WPEvent implements ProcessorInterface
 {
     
     protected $ID = "";
+
     /**
      * Adds some wordpress info to the logged data
-     * added infor are: post ID, request, post_type, query string, post title 
+     * added info are: post ID, request, post_type, query string, post title 
      * @param array $event event data
      * @return array event data
      */
     public function process(array $event)
     {
-        global $wp_query, $post, $authordata, $wp;
+        global $wp_query, $post, $authordata, $wp; //wordpress global variables //
 
-        $current_user = null; 
+        $current_user = null; //the current user for this wordpress request //
+
         if( function_exists( 'wp_get_current_user' ) ) $current_user = wp_get_current_user();
 
-        if (!isset($event['extra'])) {
+        //add an extra index to the event if not present //
+        if (!isset($event['extra'])) 
             $event['extra'] = [];
-        }
-
+        
+        //if the global $post is not empty, add some post information to the log //
         if( !empty( $post ) ){
-            $event['extra']['postID'] = $post->ID;
-            $event['extra']['postType'] = $post->post_type;
-            $event['extra']['postTitle'] = $post->post_title;
-            $event[ 'extra' ][ 'permalink' ] = get_permalink();
+            $event['extra']['postID'] = $post->ID; //post ID //
+            $event['extra']['postType'] = $post->post_type; // post type e.g. post,page, news etc //
+            $event['extra']['postTitle'] = $post->post_title; //post title //
+            $event[ 'extra' ][ 'permalink' ] = get_permalink(); //post url //
         }
 
+        // if the global $wp var is not empty, add some request info to the log //
         if( !empty( $wp ) ){
-            $event[ 'extra' ][ 'query_string' ] = $wp->query_string;
-            $event[ 'extra' ][ 'request' ] = $wp->request;
+            $event[ 'extra' ][ 'query_string' ] = $wp->query_string; //the query string for the request //
+            $event[ 'extra' ][ 'request' ] = $wp->request; //request info //
         }
         
+        //if we are on an archive request e.g. author, category, add some archive info to the log //
         if( !empty( $wp_query ) && $wp_query->is_archive ){
             $queried_object = get_queried_object();
             $event['extra']['archive'] = sprintf( '%1$s Archive: %2$s', 
@@ -50,18 +55,21 @@ class WPEvent implements ProcessorInterface
                                                 );
         }
 
+        //if the user is logged in, add some user info //
         if( $current_user instanceof \WP_User && is_user_logged_in() ){
-            $event[ 'extra' ][ 'userName' ] = $current_user->user_login;
-            $event[ 'extra' ][ 'userEmail' ] = $current_user->user_email;
-            $event[ 'extra' ][ 'userID' ] = $current_user->ID;
+            $event[ 'extra' ][ 'userName' ] = $current_user->user_login; //current username //
+            $event[ 'extra' ][ 'userEmail' ] = $current_user->user_email; //current user email address//
+            $event[ 'extra' ][ 'userID' ] = $current_user->ID; //current user ID //
         }
 
+        //if global $post var is not set yet, do some workaround //
         if( empty( $post ) ){
             $url = home_url( add_query_arg( null, $wp->request ) );
             $event['extra']['postID'] = url_to_postid( $url );
             $event[ 'extra' ][ 'permalink' ] = $url;
         }
 
+        //Add a wp filter for the $event being returned, check if we are in wordpress first //
         if( function_exists( "apply_filters" ) ) $event = apply_filters( "Zend\Log\Process\WPEvent", $event );
 
         return $event;
